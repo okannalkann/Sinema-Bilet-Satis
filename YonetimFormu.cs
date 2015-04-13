93677d5a3928f84b83895bf9915bc5e35d3b492a
okannalkann <okann.alkann@hotmail.com>
2015-04-14 00:38:37 +0300

    Revert "Revert "Baslangıc Yüklemesi""
    This reverts commit e5406f179753d92f4b137286f1a816ff9d8b1c4b.

---
+++ YonetimFormu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace sinema_bilet_satisi_EN_SON
+{
+    public partial class YonetimFormu : Form
+    {
+        public YonetimFormu()
+        {
+            InitializeComponent();
+        }
+        private void YonetimFormu_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            SaatGosterici.Start();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SaatGosterici_Tick(object sender, EventArgs e)
+        {
+            LblSaat.Text = (DateTime.Now.Hour) + ":" + (DateTime.Now.Minute) + ":" + (DateTime.Now.Second).ToString();
+        }
+
+    }
+}
